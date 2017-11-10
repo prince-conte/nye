9b972fe1505f713e7c4aa3ad6985673bc8f3c9e9
Alexander Kozlov <alexabderl@coalla.ru>
2017-11-10 11:16:33 +0300

    fix + project-page+

--- dist/js/application.js
+++ dist/js/application.js
@@ -382,17 +382,4 @@ $(function () {
         sliderInit();
     });
 });
-'use strict';
-
-var map;
-
-function initialize() {
-    var myLatlng = new google.maps.LatLng(-25.363882, 131.044922);
-    var mapOptions = {
-        zoom: 4,
-        center: myLatlng
-    };
-    map = new google.maps.Map(document.getElementById('project-map'), mapOptions);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+"use strict";
